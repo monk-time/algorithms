faebba0e44eec8efc8ae09baec48013bf0308ad6
Dmitry <monk-time@users.noreply.github.com>
2023-05-24 00:13:13 +0300

    Rewrite task i using a list

--- sprint2/i_limited_queue.py
+++ sprint2/i_limited_queue.py
@@ -1,7 +1,7 @@
 from collections import deque
 
 
-class MyQueueSized:
+class MyQueueSizedDeque:
     def __init__(self, max_size: int):
         self.max_size = max_size
         self._queue = deque()
@@ -24,6 +24,38 @@ class MyQueueSized:
 
     def size(self):
         return len(self._queue)
+
+
+class MyQueueSized:
+    def __init__(self, max_size: int):
+        self.max_size = max_size
+        self.queue = [None] * max_size
+        self.index_first = self.index_last = 0
+        self.queue_size = 0
+
+    def push(self, value: int):
+        if self.queue_size >= self.max_size:
+            return 'error'
+        self.queue[self.index_last] = value
+        self.index_last = (self.index_last + 1) % self.max_size
+        self.queue_size += 1
+        return None
+
+    def pop(self):
+        if not self.queue_size:
+            return 'None'
+        value = self.queue[self.index_first]
+        self.index_first = (self.index_first + 1) % self.max_size
+        self.queue_size -= 1
+        return value  # noqa
+
+    def peek(self):
+        if not self.queue_size:
+            return 'None'
+        return self.queue[self.index_first]
+
+    def size(self):
+        return self.queue_size
 
 
 def execute(command: str, queue: MyQueueSized) -> None:
