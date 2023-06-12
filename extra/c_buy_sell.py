5102fb40c8028ada3f37cf2e695f6736a669cf7b
Dmitry <monk-time@users.noreply.github.com>
2023-06-12 20:39:09 +0300

    Finish task c

--- extra/c_buy_sell.py
+++ extra/c_buy_sell.py
@@ -1,22 +1,19 @@
-from random import randint
-
-
 def find_max_difference(a: list[int], money: int = 1000) -> tuple[int, int]:
-    max_i, max_j, max_diff = 0, 0, 0
-    for i in range(len(a) - 1):
-        for j in range(i + 1, len(a)):
-            if a[i] > money:
-                continue
-            gas = money // a[i]
-            new_diff = gas * (a[j] - a[i])
-            if a[i] <= money and new_diff > max_diff:
-                max_i, max_j = i + 1, j + 1
-                max_diff = new_diff
-    return max_i, max_j
+    max_left, max_right, max_profit_coef = 0, 0, 1
+    left = right = 0
+    for i in range(1, len(a)):
+        if a[i] < a[left]:
+            left = i
+        elif a[i] > a[left] and (right <= left or a[i] > a[right]):
+            right = i
+            profit_coef = a[right] / a[left]
+            if a[left] <= money and profit_coef > max_profit_coef:
+                max_left, max_right = left + 1, right + 1
+                max_profit_coef = profit_coef
+    return max_left, max_right
 
 
 if __name__ == '__main__':
     n = int(input())
     numbers = list(map(int, input().split()))
-    # numbers = [randint(1, 5001) for _ in range(100_000)]
     print(*find_max_difference(numbers), sep=' ')
