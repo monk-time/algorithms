ac252302c0bbc609b0c689c1129cf3902ea21732
Dmitry <monk-time@users.noreply.github.com>
2023-06-13 18:51:44 +0300

    Redo task a

--- extra/a_steps.py
+++ extra/a_steps.py
@@ -1,9 +1,16 @@
-def steps(x: int) -> int:
+import math
+
+
+def steps_iter(x: int) -> int:
     n, total = 0, 0
     while total <= x:
         n += 1
         total += n
     return n - 1
+
+
+def steps(x: int) -> int:
+    return math.floor((math.sqrt(8 * x + 1) - 1) / 2)
 
 
 if __name__ == '__main__':
