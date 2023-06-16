83680a8713622063bf93062a5bbaf86856c700bb
Dmitry <monk-time@users.noreply.github.com>
2023-06-16 19:56:43 +0300

    Faster task M

--- sprint3/m_golden_mean.py
+++ sprint3/m_golden_mean.py
@@ -1,22 +1,18 @@
-from typing import List
-
-
-def median(a: List[int]) -> int:
-    half = len(a) // 2
-    if len(a) % 2 != 0:
-        return a[half]
-    if a[half - 1] == a[half]:
+def median(a):
+    half, remainder = divmod(len(a), 2)
+    if remainder != 0 or a[half - 1] == a[half]:
         return a[half]
     avg, remainder = divmod(a[half - 1] + a[half], 2)
-    return avg if not remainder else avg + .5
+    return avg if not remainder else avg + 0.5
 
 
-def median_two(a: List[int], b: List[int]) -> int:
+def median_two(a, b):
     a_left, a_right = 0, len(a) - 1
     b_left, b_right = 0, len(b) - 1
     while a_left <= a_right and b_left <= b_right:
         if a_left == a_right and b_left == b_right:
-            return median([a[a_left], b[b_left]])
+            avg, remainder = divmod(a[a_left] + b[b_left], 2)
+            return avg if not remainder else avg + 0.5
         if a[a_left] <= b[b_left]:
             a_left += 1
         else:
@@ -34,6 +30,6 @@ def median_two(a: List[int], b: List[int]) -> int:
 
 if __name__ == '__main__':
     _, _ = input(), input()
-    list_a = list(map(int, input().split()))
-    list_b = list(map(int, input().split()))
-    print(median_two(list_a, list_b))
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    print(median_two(a, b))
