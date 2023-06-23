016d67e59ea509dfe850706d376b22fde7b4b122
Dmitry <monk-time@users.noreply.github.com>
2023-06-23 15:21:55 +0300

    Small fixes

--- bonus2/n_tree_partition.py
+++ bonus2/n_tree_partition.py
@@ -23,15 +23,12 @@ def split(
         return None, root
     if k >= root.size:
         return root, None
-    if not root.left:
-        small_root = root
-        small_root.right, root = split(root.right, k - 1)
-        small_root.size -= root.size if root else 0
-    elif root.left.size >= k:
+    if root.left and root.left.size >= k:
         small_root, root.left = split(root.left, k)
         root.size -= k
     else:
         small_root = root
-        small_root.right, root = split(root.right, k - root.left.size - 1)
+        left_size = root.left.size if root.left else 0
+        small_root.right, root = split(root.right, k - left_size - 1)
         small_root.size -= root.size if root else 0
     return small_root, root
--- shbr2/e_deleting_numbers.py
+++ shbr2/e_deleting_numbers.py
@@ -4,11 +4,11 @@ from itertools import pairwise
 
 def min_to_delete(a: list[int]) -> int:
     counter = Counter(a)
-    keys = sorted(counter.keys())
-    if len(keys) < 2:
+    unique_nums = sorted(counter.keys())
+    if len(unique_nums) < 2:
         return 0
     max_len = 1
-    for x, y in pairwise(keys):
+    for x, y in pairwise(unique_nums):
         if abs(x - y) <= 1:
             max_len = max(counter[x] + counter[y], max_len)
     return len(a) - max_len
