0b49f5f42e81583ae91ce5b28cd161d7cf0f9ee9
Dmitry <monk-time@users.noreply.github.com>
2023-07-14 16:12:49 +0300

    Improve binary search

--- sprint3/l_two_bicycles.py
+++ sprint3/l_two_bicycles.py
@@ -20,15 +20,13 @@ DAY_OFFSET = 1
 
 def left_binary_search(a: List[int], k: int) -> int:
     left, right = 0, len(a) - 1
-    while left <= right:
+    while left < right:
         mid = (left + right) // 2
         if a[mid] >= k:
-            if left == mid:
-                return mid + DAY_OFFSET
             right = mid
         else:
             left = mid + 1
-    return -1
+    return left + DAY_OFFSET if a and a[left] >= k else -1
 
 
 def left_binary_search_rec(a: List[int], k: int) -> int:
