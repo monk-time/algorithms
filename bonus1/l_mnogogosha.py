6e031dafde8b68152b37c91a0a7c33057ed43f5b
Dmitry <monk-time@users.noreply.github.com>
2024-09-01 20:12:21 +0300

    Linting

--- bonus1/l_mnogogosha.py
+++ bonus1/l_mnogogosha.py
@@ -21,9 +21,9 @@ def substrings_k_times(s: str, n: int, k: int) -> list[int]:
         hash_1 = ((hash_1 - ch_prev * q_1_big) * q_1 + ch_next) % m_1
         hash_2 = ((hash_2 - ch_prev * q_2_big) * q_2 + ch_next) % m_2
         if (hash_1, hash_2) not in counter:
-            counter[(hash_1, hash_2)] = [i, 1]
+            counter[hash_1, hash_2] = [i, 1]
         else:
-            counter[(hash_1, hash_2)][1] += 1
+            counter[hash_1, hash_2][1] += 1
     return [i for i, count in counter.values() if count >= k]
 
 
